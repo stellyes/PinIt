4bd9e732499119e82cedfcaeea3e6302cc6eec73
Jason Savoie <flimits@gmail.com>
2023-11-08 11:32:55 -0800

    pushing up mysql connections for local and heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -8,5 +8,14 @@ if (process.env.JAWSDB_URL) {
 } else {
     sequelize = new sequelize(
         process.env.DB_NAME,
-    )
+        process.env.DB_PASSWD,
+        process.env.DB_USER,
+        {
+            host: '127.0.0.1',
+            dialect: 'mysql',
+            port: 3306
+        }
+    );
 }
+
+module.exports = sequelize;
