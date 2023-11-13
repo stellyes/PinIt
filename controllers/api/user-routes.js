0fbb25a06fdc073295959156bf748c5a88ea50b9
Ryan England <me@rengland.org>
2023-11-12 19:00:17 -0800

    Added login route to api routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
 const { User, Location, Category } = require("../../models");
 
 // /api/users/ routes
@@ -37,7 +38,7 @@ router.get("/:id", async (req, res) => {
 });
 
 // Create new user
-router.post("/", async (req, res) => {
+router.post("/signup", async (req, res) => {
     try {
         const userData = await User.create(req.body);
         res.status(200).json(userData);
@@ -89,6 +90,53 @@ router.delete("/:id", async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+// Login for user
+router.post("/login", async (req, res) => {
+    try {
+      const userData = await User.findOne({
+        where: {
+          email: req.body.email,
+        },
+      });
+  
+      if (!userData) {
+        res
+          .status(404)
+          .json(
+            { 
+                code: 404, 
+                message: "Login failed. Incorrect email/password" 
+        });
+        return;
+      }
+  
+      // Validate password against encrypted version
+      const validPassword = await bcrypt.compare(
+        req.body.password,
+        userData.password
+      );
+  
+      if (!validPassword) {
+        res
+          .status(400)
+          .json(
+            { 
+                code: 400, 
+                message: "Login failed. Incorrect email/password" 
+        });
+        return;
+      }
+  
+      // Set req.session.loggedIn to userData.id so that 
+      // home-routes can access locations from logged in user
+      req.session.loggedIn = userData.id;
+      
+      res.status(200).json({ code: 200 });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
     
 module.exports = router;
     
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,12 +1,17 @@
 const router = require("express").Router();
-const { User, Category, Location } = require("../models");
+const { Location } = require("../models");
 const withAuth = require("../utils/withAuth");
 
 // Homepage route
 router.get("/", withAuth, async (req, res) => {
   try {
     // Get locations
-    const locationData = Location.findAll();
+    const locationData = Location.findAll({
+      where: {
+        // user_id is stored as req.sessions.loggedIn
+        user_id: req.session.loggedIn
+      }
+    });
 
     // Map location data
     const locations = locationData.map((location) => location.get({ plain: true }));
@@ -18,12 +23,13 @@ router.get("/", withAuth, async (req, res) => {
     });
     
   } catch(err) {
-    res.status(500).json(err);
+    res.send("<h1>500 Internal Server Error</h1>");
   }
-})
+});
 
 // Login route
 router.get('/login', (req, res) => {
+  try {
     // If user is logged in, redirect to main page
     if (req.session.loggedIn) {
       res.redirect('/dashboard');
@@ -31,4 +37,9 @@ router.get('/login', (req, res) => {
     }
 
     res.render('login');
+  } catch {
+    res.send("<h1>500 Internal Server Error</h1>");
+  }
 });
+
+module.exports = router;
